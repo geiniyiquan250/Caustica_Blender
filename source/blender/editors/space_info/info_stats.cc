06705f2ecff62f046bf87096dc78b3673d7b26d4
Shuime <994484425@qq.com>
2026-09-04 11:20:57 +0800

    Brand Shuimeng custom build in status bar

--- source/blender/editors/space_info/info_stats.cc
+++ source/blender/editors/space_info/info_stats.cc
@@ -829,7 +829,7 @@ const char *ED_info_statusbar_string_ex(Main *bmain,
       ofs += BLI_snprintf_utf8_rlen(info + ofs, len - ofs, " | ");
     }
     ofs += BLI_snprintf_utf8_rlen(
-        info + ofs, len - ofs, IFACE_("%s"), BKE_blender_version_string_compact());
+        info + ofs, len - ofs, IFACE_("%s Shuimeng个人定制版"), BKE_blender_version_string_compact());
   }
 
   return info;
